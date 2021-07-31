25753675224c70a82a1efcc4c8197286ca538370
fanshe <fanshe@tencent.com>
2021-07-31 15:55:43 +0800

    init

---
+++ DataBindingListView/app/src/main/java/com/tme/lcore/databindinglistview/RcyViewModel.kt
@@ -0,0 +1,14 @@
+package com.tme.lcore.databindinglistview
+
+import androidx.databinding.BaseObservable
+import androidx.databinding.ObservableField
+
+
+/**
+ * @author fanshe
+ * @date 2021/7/31
+ */
+class RcyViewModel {
+    var name: ObservableField<String> = ObservableField<String>()
+    var age: ObservableField<String> = ObservableField<String>()
+}
